33ac04fcfcef3ad6f6f1ab07426a790d27dfed06
MustkeemArsh10 <92543062+MustkeemArsh10@users.noreply.github.com>
2023-02-13 00:11:04 +0530

    Create Commands.sh

---
+++ Commands.sh
@@ -0,0 +1,31 @@
+# Linux-Commands
+basic Linux commands 
+
+pwd - print working directory
+ls - list 
+cd - change directory to
+.. - use to back
+sudo - to take permission
+cat - to concatenate any file
+cp - copy anything 
+rm - remove 
+whoami - find your user account
+apt - 
+ipconfig - to find your IP address
+sudo bash - to switch to administrator mode
+clear or ctrl+l - Clear screen
+ctrl+c - stop any process
+adduser - adding other user
+ps - process status
+su root - switch user to root 
+su - switch user
+mdkir -
+sudo apt get update -
+--help - just help
+lsusb - list all the usb connected
+man - to get the maneul of nay command
+apropos - helps to find any command or help to find the coreect command 
+dpkg - depackage
+apt - Advance package Tool 
+sudo apt update 
+apt --fix-broken install
